7e754609ca7332a57e3e4c2104e221a9e8a5134e
Eric Boittier <eric.boittier@icloud.com>
2026-03-30 11:54:41 +0200

    Update CLI script to change resource command from CYBZ to PCRO for improved simulation setup.

--- cli/01_make_res_cli.sh
+++ cli/01_make_res_cli.sh
@@ -7,5 +7,5 @@ set -e
 
 echo "=== 01: make_res (CLI) ==="
 echo "Command: mmml make-res --res CYBZ --skip-energy-show"
-mmml make-res --res CYBZ --skip-energy-show
+mmml make-res --res PCRO --skip-energy-show
 echo "Output: pdb/initial.pdb, psf/initial.psf, xyz/initial.xyz"
